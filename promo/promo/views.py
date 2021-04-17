140d77b3c8fc4038d847c27e523e89cdb01a7828
P4elme6ka <006gta1@gmail.com>
2021-04-18 01:40:27 +0300

    downloader fix

--- promo/promo/views.py
+++ promo/promo/views.py
@@ -1,5 +1,7 @@
 from django.template import loader
 from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
@@ -14,9 +16,11 @@ def features(request):
 def uploader(request):
     if request.method == "GET":
         templ = loader.get_template("promo/uploader.html")
-
-        return HttpResponse(templ.render(None, request))
-    elif request.method == "POST":
+        return HttpResponse(templ.render({}, request))
+    elif request.method == "POST" and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage("../data/")
+        filename = fs.save(myfile.name, myfile)
         templ = loader.get_template("promo/index.html")
         va = {
             "show_popup": True,
